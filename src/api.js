ae47605501f65f83ca1304618748eac39b170b1b
Thirukumaran25 <kumaran2597@gmail.com>
2025-09-10 11:25:33 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api/';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://djangodrfweek1test1.onrender.com/api/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
